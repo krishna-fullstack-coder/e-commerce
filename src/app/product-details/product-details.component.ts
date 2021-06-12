f9fce8bd45284682b8b5c28b55c5d523d2442ef6
krishna-fullstack-coder <krishna.fullstack.coder@gmail.com>
2021-06-12 18:31:39 +0530

    Added product details

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,// service
+  ) { }
+
+  ngOnInit() {
+    // First get the product id from the current route.
+    // route.snapshot, which is the ActivatedRouteSnapshot that contains information about the active route at that particular moment 
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+}
